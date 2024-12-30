8aeb673263f731be402d5240eace686b114b3075
Diver0218 <jamin.bareis@gmail.com>
2024-12-30 14:49:00 +0100

    added library includes and defines to bluetooth header file

--- include/Bluetooth.h
+++ include/Bluetooth.h
@@ -1,5 +1,12 @@
+#include <esp_bt.h>
+#include <esp_bt_main.h>
+#include <esp_sleep.h>
+#include <BLEDevice.h>
 #include <BLEServer.h>
 #include "iLock.h"
+
+#define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
 class Bluetooth {
 
