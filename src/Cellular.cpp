273e73ddfdb50bd53885ad5a4ea1aea47b4d0ee8
Diver0218 <jamin.bareis@gmail.com>
2025-03-08 14:39:16 +0100

    working cellular module

--- src/Cellular.cpp
+++ src/Cellular.cpp
@@ -13,14 +13,16 @@ void Cellular::connect()
     modem = new HardwareSerial(2);
     modem->begin(9600, SERIAL_8N1, rx, tx);
 
+    sendSerial("AT+CFUN=1,1", 4000);                              // Modem einschalten
+    sendSerial("AT+CPIN=\"" + pin + "\"");                  // PIN setzen
     sendSerial("AT+CGATT=1");                               // GPRS-Anmeldung erzwingen
     sendSerial("AT+SAPBR=0,1");                             // GPRS-Verbindung löschen   
     sendSerial("AT+SAPBR=3,1,\"Contype\",\"GPRS\"");        // GPRS-Verbindungstyp setzen
     sendSerial(("AT+SAPBR=3,1,\"APN\",\"" + apn + "\"").c_str());     // APN konfigurieren
     sendSerial(("AT+SAPBR=3,1,\"USER\",\"" + user + "\"").c_str());   // Benutzername konfigurieren
     sendSerial(("AT+SAPBR=3,1,\"PWD\",\"" + pass + "\"").c_str());    // Passwort konfigurieren
-    sendSerial("AT+SAPBR=1,1", 2000);                             // GPRS-Verbindung öffnen
-    sendSerial("AT+SAPBR=2,1", 3000);                       // Verbindung prüfen
+    sendSerial("AT+SAPBR=1,1", 2000);                       // GPRS-Verbindung öffnen
+    sendSerial("AT+SAPBR=2,1", 2000);                       // Verbindung prüfen
 
     Serial.println("Cellular connected to the internet");
 }
@@ -29,6 +31,8 @@ void Cellular::disconnect()
 {
     sendSerial("AT+SAPBR=0,1");                             // GPRS-Verbindung löschen
     sendSerial("AT+CGATT=0");                               // GPRS-Anmeldung beenden
+    sendSerial("AT+CFUN=0");                            // Modem in den Schlafmodus versetzen
+    delete modem;
     xSemaphoreGive(gsm_semaphore);
 }
 
@@ -63,6 +67,14 @@ int Cellular::post(std::string url, std::string payload)
     sendSerial(payload.c_str());
     sendSerial("AT+HTTPACTION=1");                          // HTTP-GET ausführen
     std::string response = sendSerial("AT+HTTPREAD");       // Antwort lesen
+    size_t pos = response.find(":");
+    if (pos != std::string::npos) {
+        response = response.substr(pos + 1);
+        pos = response.find(",");
+        if (pos != std::string::npos) {
+            response = response.substr(0, pos);
+        }
+    }
     sendSerial("AT+HTTPTERM");                              // HTTP-Session beenden
     return atoi(response.c_str());
 }
