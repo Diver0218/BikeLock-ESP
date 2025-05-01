4cde22c2ad67ebf3b448e5f31a1d04296c88d2c6
Diver0218 <jamin.bareis@gmail.com>
2025-05-01 12:53:06 +0200

    Reduziere vTaskDelay in der toggleLock-Methode auf 2000 ms für schnellere Reaktionszeiten

--- src/Lock.cpp
+++ src/Lock.cpp
@@ -11,14 +11,14 @@ Lock::Lock(iMotor *lockMotor, iMotor *safetyMotor, int steps_to_lock){
 void Lock::toggleLock(){
     if (locked) {
         safetyMotor->rotate(-1 * steps_to_lock);
-        vTaskDelay(3000 / portTICK_PERIOD_MS);
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
         locked = false;
         isLocked_global = false;
         lockMotor->rotate(-1 * steps_to_lock);
         Serial.println("Unlocked");
     } else {
         lockMotor->rotate(steps_to_lock);
-        vTaskDelay(3000 / portTICK_PERIOD_MS);
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
         locked = true;
         isLocked_global = true;
         Serial.println("Locked");
